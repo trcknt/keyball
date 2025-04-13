3b003f301499eaa8db0e0584a983c627db50a530
trcknt <101651952+trcknt@users.noreply.github.com>
2025-04-13 16:47:13 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -45,3 +45,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 5
+#define AUTO_MOUSE_TIME 800 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
